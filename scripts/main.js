3c86744bf0138f88522f38e05b9ae2ac1ada0c2e
Cont Activ <dancozianu@yahoo.com>
2017-02-27 15:16:42 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -33,27 +33,3 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/contactiv.jpg');
     }
 }
-
-/* quoting */
-    var quotePara = document.querySelector('section p');
-    var quoteJson;
-    getQuotes("https://github.com/dancozianu/codecademy.github.io/blob/master/quotes.json", populateJson);
-    var intervalID = window.setInterval(showQuote, 10000);
-    function getQuotes("https://github.com/dancozianu/codecademy.github.io/blob/master/quotes.json", callback) {
-      var request = new XMLHttpRequest();
-      request.open('GET', "https://github.com/dancozianu/codecademy.github.io/blob/master/quotes.json");
-      request.responseType = 'json';
-      request.send();
-      request.onreadystatechange = function() {
-        if (request.readyState === 4 && request.status === 200) {
-          callback(request.response);
-        }
-      };
-    }
-    function populateJson(response) {
-      quoteJson = response;
-    }
-    function showQuote() {
-      var random = Math.floor((Math.random()*25));
-      quotePara.textContent = quoteJson[random].quote + ' -- ' + quoteJson[random].author;
-    }
