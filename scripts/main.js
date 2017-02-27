3b0511cf9e4761771b16d7cf6178240162ee87f6
Cont Activ <dancozianu@yahoo.com>
2017-02-27 14:28:58 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -33,3 +33,27 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/contactiv.jpg');
     }
 }
+
+/* quoting */
+    var quotePara = document.querySelector('section p');
+    var quoteJson;
+    getQuotes('quotes.json', populateJson);
+    var intervalID = window.setInterval(showQuote, 10000);
+    function getQuotes(url, callback) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url);
+      request.responseType = 'json';
+      request.send();
+      request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+          callback(request.response);
+        }
+      };
+    }
+    function populateJson(response) {
+      quoteJson = response;
+    }
+    function showQuote() {
+      var random = Math.floor((Math.random()*25));
+      quotePara.textContent = quoteJson[random].quote + ' -- ' + quoteJson[random].author;
+    }
